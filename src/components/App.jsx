ac741ccff84764b1c3a4d98cbdfaa277c1717af9
pragmaticLudusian <arsen.r.m@gmail.com>
2025-04-26 21:38:10 +0300

    feat: add route wildcards for logged in/out users

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,42 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import "./styles/App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <Routes>
-      <Route path="/ducks" element={<Ducks />} />
-      <Route path="/my-profile" element={<MyProfile />} />
+      <Route path='/ducks' element={<Ducks />} />
+      <Route path='/my-profile' element={<MyProfile />} />
       <Route
-        path="/login"
+        path='/login'
         element={
-          <div className="loginContainer">
+          <div className='loginContainer'>
             <Login />
           </div>
         }
       />
       <Route
-        path="/register"
+        path='/register'
         element={
-          <div className="registerContainer">
+          <div className='registerContainer'>
             <Register />
           </div>
+        }
+      />
+      <Route
+        path='*'
+        element={
+          isLoggedIn ? (
+            <Navigate to='/ducks' replace />
+          ) : (
+            <Navigate to='/login' replace />
+          )
         }
       />
     </Routes>
