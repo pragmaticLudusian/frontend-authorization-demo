409bec1bb28c8a64abec928d0df1ce2932171b76
pragmaticLudusian <arsen.r.m@gmail.com>
2025-04-26 22:33:42 +0300

    feat: add wrapper compo and protect routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,33 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
+import ProtectedRoute from "./ProtectedRoute";
 import "./styles/App.css";
-import { useState } from "react";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <Routes>
-      <Route path='/ducks' element={<Ducks />} />
-      <Route path='/my-profile' element={<MyProfile />} />
+      <Route
+        path='/ducks'
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <Ducks />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/my-profile'
+        element={
+          <ProtectedRoute isLoggedIn={isLoggedIn}>
+            <MyProfile />
+          </ProtectedRoute>
+        }
+      />
       <Route
         path='/login'
         element={
