24a36837e33e46ee39f41a15512bfaef1e08bbad
pragmaticLudusian <arsen.r.m@gmail.com>
2025-04-27 17:16:25 +0300

    implement register, testing authentication api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,32 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Ducks from "./Ducks";
 import Login from "./Login";
 import MyProfile from "./MyProfile";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
+import * as auth from "../utils/auth";
 import "./styles/App.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRegistration = ({
+    username,
+    email,
+    password,
+    confirmPassword,
+  }) => {
+    if (password === confirmPassword) {
+      auth
+        .register(username, password, email)
+        .then(() => {
+          navigate("/login");
+        })
+        .catch(console.error);
+    }
+  };
 
   return (
     <Routes>
@@ -40,7 +58,7 @@ function App() {
         path='/register'
         element={
           <div className='registerContainer'>
-            <Register />
+            <Register handleRegistration={handleRegistration} />
           </div>
         }
       />
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Logo from "./Logo";
 import "./styles/Register.css";
 
-const Register = () => {
+const Register = ({ handleRegistration }) => {
   const [data, setData] = useState({
     username: "",
     email: "",
@@ -19,52 +19,57 @@ const Register = () => {
     }));
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleRegistration(data);
+  };
+
   return (
-    <div className="register">
+    <div className='register'>
       <Logo title={"CryptoDucks"} />
-      <p className="register__welcome">Please register.</p>
-      <form className="register__form">
-        <label htmlFor="username">Username:</label>
+      <p className='register__welcome'>Please register.</p>
+      <form className='register__form' onSubmit={handleSubmit}>
+        <label htmlFor='username'>Username:</label>
         <input
-          id="username"
-          name="username"
-          type="text"
+          id='username'
+          name='username'
+          type='text'
           value={data.username}
           onChange={handleChange}
         />
-        <label htmlFor="email">Email:</label>
+        <label htmlFor='email'>Email:</label>
         <input
-          id="email"
-          name="email"
-          type="email"
+          id='email'
+          name='email'
+          type='email'
           value={data.email}
           onChange={handleChange}
         />
-        <label htmlFor="password">Password:</label>
+        <label htmlFor='password'>Password:</label>
         <input
-          id="password"
-          name="password"
-          type="password"
+          id='password'
+          name='password'
+          type='password'
           value={data.password}
           onChange={handleChange}
         />
-        <label htmlFor="confirmPassword">Confirm password:</label>
+        <label htmlFor='confirmPassword'>Confirm password:</label>
         <input
-          id="confirmPassword"
-          name="confirmPassword"
-          type="password"
+          id='confirmPassword'
+          name='confirmPassword'
+          type='password'
           value={data.confirmPassword}
           onChange={handleChange}
         />
-        <div className="register__button-container">
-          <button type="submit" className="register__link">
+        <div className='register__button-container'>
+          <button type='submit' className='register__link'>
             Sign up
           </button>
         </div>
       </form>
-      <div className="register__signin">
+      <div className='register__signin'>
         <p>Already a member?</p>
-        <Link to="login" className="register__login-link">
+        <Link to='login' className='register__login-link'>
           Log in here
         </Link>
       </div>
